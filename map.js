eaafdd371f2d99c9663a5ce53b1766828216b1b7
hookerslayer <148587686+hookerslayer@users.noreply.github.com>
2026-05-07 15:45:43 +0300

    Update map.js

--- map.js
+++ map.js
@@ -23,11 +23,40 @@ const imageLayer = new ol.layer.Image({
     })
 });
 
+// Слой провинций
+const provincesLayer = new ol.layer.Vector({
+
+    source: new ol.source.Vector({
+
+        url: './provinces.geojson',
+
+        format: new ol.format.GeoJSON()
+
+    }),
+
+    style: new ol.style.Style({
+
+        stroke: new ol.style.Stroke({
+            color: 'red',
+            width: 1
+        }),
+
+        fill: new ol.style.Fill({
+            color: 'rgba(255, 0, 0, 0.5)'
+        })
+
+    })
+
+});
+
 // Создаем карту
 const map = new ol.Map({
     target: 'map',
 
-    layers: [imageLayer],
+    layers: [
+        imageLayer,
+        provincesLayer
+    ]
 
     view: new ol.View({
         projection: projection,
@@ -60,4 +89,55 @@ map.on('pointermove', function(event) {
 
     // Обновляем текст
     coordsDiv.innerHTML = `X: ${x} | Y: ${y}`;
+});
+
+// Создаем HTML элемент popup
+const popupElement = document.createElement('div');
+
+popupElement.style.position = 'absolute';
+popupElement.style.background = 'white';
+popupElement.style.padding = '6px 10px';
+popupElement.style.border = '1px solid black';
+popupElement.style.borderRadius = '6px';
+popupElement.style.minWidth = '80px';
+
+// Создаем overlay
+const popup = new ol.Overlay({
+    element: popupElement,
+    positioning: 'bottom-center',
+    stopEvent: false,
+    offset: [0, -10]
+});
+
+map.addOverlay(popup);
+
+// Клик по карте
+map.on('click', function(event) {
+
+    // Ищем объект под курсором
+    const feature = map.forEachFeatureAtPixel(
+        event.pixel,
+        function(feature) {
+            return feature;
+        }
+    );
+
+    // Если объект найден
+    if (feature) {
+
+        // Получаем ID
+        const provinceId = feature.get('id');
+
+        // Текст popup
+        popupElement.innerHTML = `Province ID: ${provinceId}`;
+
+        // Показываем popup
+        popup.setPosition(event.coordinate);
+    }
+
+    // Если кликнули мимо
+    else {
+        popup.setPosition(undefined);
+    }
+
 });
