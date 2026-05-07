f8f09c7aa3a6a20f46dbd38e01984d6079acdba4
hookerslayer <148587686+hookerslayer@users.noreply.github.com>
2026-05-07 13:41:25 +0300

    Update map.js

--- map.js
+++ map.js
@@ -42,3 +42,22 @@ const map = new ol.Map({
         extent: extent
     })
 });
+
+// Блок вывода координат
+const coordsDiv = document.getElementById('coords');
+
+// Отслеживаем движение мыши по карте
+map.on('pointermove', function(event) {
+
+    // Координаты курсора в системе карты
+    const coords = event.coordinate;
+
+    // X координата
+    const x = Math.round(coords[0]);
+
+    // Y координата
+    const y = Math.round(coords[1]);
+
+    // Обновляем текст
+    coordsDiv.innerHTML = `X: ${x} | Y: ${y}`;
+});
